72aedc200bcd73720b2e43fe58608ddfc7195102
Salam337 <salamadash@gmail.com>
2022-10-31 16:39:40 -0400

    modified main page and navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
+import React from 'react'
+import './home.css'
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
 	return (
 		<>
-			<h2>Home Page</h2>
+		<div id='main-page-body'>
+			<h2 id='global-text'>Global Online</h2>
+			<img id='img' src='https://www.freeiconspng.com/thumbs/globe-png/globe-png-hd-1.png' />
+			<div id="search-box">
+				<input id="input"></input>
+				<button id="search">Search</button>
+			</div>
+		</div>
 		</>
 	)
 }
