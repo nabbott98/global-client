059d3945db820f6cf7b73246aac28144c86f9ea1
JChenoweth <jchenoweth@Jennifers-MBP.local>
2022-11-01 09:32:41 -0500

    current version of indexitems

---
+++ src/api/item.js
@@ -0,0 +1,52 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const itemCreate = (data, user) => {
+	return axios({
+		method: 'POST',
+		url: apiUrl + '/items',
+		data: {
+			item: data,
+		},
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
+
+export const itemIndex = (user) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/items'
+	})
+}
+
+export const itemShow = (user, id) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/items/' + id
+	})
+}
+
+export const itemUpdate = (data, user, id) => {
+	return axios({
+		method: 'PATCH',
+		url: apiUrl + '/items/' + id,
+		data: {
+			item: data,
+		},
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
+
+export const itemDelete = (user, id) => {
+	return axios({
+		method: 'DELETE',
+		url: apiUrl + '/items/' + id,
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
---
+++ src/components/Items/ItemIndex.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react' 
+import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { itemIndex } from '../../api/item'
+
+const cardContainerLayout = {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    justifyContent: 'center'
+}
+
+const ItemIndex = ({ user, msgAlert }) => {
+
+    const [allItems, setAllItems] = useState([])
+
+    useEffect(() => {
+        itemIndex(user)
+        .then(res => {
+            setAllItems(res.data.items)
+        })
+        .catch((error) => {
+            msgAlert({
+                heading: 'Failure',
+                message: 'Index Pets Failure' + error,
+                variant: 'danger'
+            })
+        })
+    }, [])
+
+    const itemCards = allItems.map(item => (
+        <Card key={ item.id } style={{ width: '30%', margin: 5 }}>
+            <Card.Header>{ item.fullTitle }</Card.Header>
+            <Card.Body>
+                <Card.Text>
+                    <Link to={ `/items/${item.id}` }>View { item.name }</Link>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    ))
+
+    return (
+        <div className='container-md' style={ cardContainerLayout }>
+            { itemCards }
+        </div>
+    )
+}
+
+export default ItemIndex
